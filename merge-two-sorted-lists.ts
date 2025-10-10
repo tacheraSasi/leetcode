a1390f5726263327d405e0849a4c11ceb135496b
Tachera Sasi <tacherasasi@gmail.com>
2025-10-10 15:28:23 +0300

    Refactor mergeTwoLists function and improve ListNode initialization

--- merge-two-sorted-lists.ts
+++ merge-two-sorted-lists.ts
@@ -5,12 +5,32 @@ class ListNode {
          this.val = (val===undefined ? 0 : val)
          this.next = (next===undefined ? null : next)
      }
- }
+}
 
+const list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+const list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
 
 function mergeTwoLists(
   list1: ListNode | null,
   list2: ListNode | null
 ): ListNode | null {
-    
+  let dummy = new ListNode(0);
+  let current = dummy;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+
+  current.next = list1 || list2;
+
+  return dummy.next;
 }
+console.log(mergeTwoLists(list1, list2));
+
