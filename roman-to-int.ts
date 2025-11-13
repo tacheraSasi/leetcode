a41dfe4465b6f103254df8444b19de68054dc21d
Tachera Sasi <tacherasasi@gmail.com>
2025-11-13 10:54:12 +0300

    Update roman-to-int.ts

--- roman-to-int.ts
+++ roman-to-int.ts
@@ -1,5 +1,14 @@
 export default function romanToInt(roman: string) {
   console.log("Roman", roman);
+
+  const s = roman.toUpperCase();
+  for (let i = 0; i < s.length; i++) {
+    const curr = s[i];
+    const next = i + 1 < s.length ? s[i + 1] : 0;
+
+    const currVal = romanNumerals[curr];
+    const nextVal = romanNumerals[next];
+  }
 }
 
 const romanNumerals: { [Key: string]: number } = {
